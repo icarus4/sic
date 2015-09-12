bae5187bec72353ef2f6133a759e27360e79ba99
icarus4 <icarus4.chu@gmail.com>
2015-09-12 14:26:17 +0800

    1. Integrate Capistrano 2. Exception handling

--- app/services/parse_statement_by_stock.rb
+++ app/services/parse_statement_by_stock.rb
@@ -15,12 +15,22 @@ class ParseStatementByStock
             statement = Statement.find_by(stock_id: meta.stock.id, year: year, quarter: quarter, statement_type: type)
             next if statement.try(:parsed_at)
 
-            result = Statement::TwseStatement.new(meta).parse
+            begin
+              tries ||= 0
+              result = Statement::TwseStatement.new(meta).parse
+              raise RuntimeError
+            rescue => e
+              puts "Stop for a while"
+              tries += 1
+              sleep (30 + tries*10)
+              retry if tries < 10
+            end
+
             if result.nil?
               failed_count += 1
               puts "No result, type:#{type} year:#{year} quarter:#{quarter}"
               throw(:no_result) if failed_count > 3
-              sleep 1
+              sleep 2
             end
           end
         end
