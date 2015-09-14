773db9079b479705bc6237918556d383609ef491
icarus4 <icarus4.chu@gmail.com>
2015-09-14 10:54:27 +0800

    Reverse performance tuning

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,20 +5,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find params[:id]
-    item_children_ids = @item.children.pluck(:id)
-    item_mappings = ItemMapping.where(item_id: item_children_ids)
-    @item_mappings = {}
-    item_mappings.each do |item_mapping|
-      @item_mappings[item_mapping.item_id.to_s] ||= { stock_ids: [], statement_ids: [] }
-      @item_mappings[item_mapping.item_id.to_s][:stock_ids] << item_mapping.stock_id
-      @item_mappings[item_mapping.item_id.to_s][:statement_ids] << item_mapping.statement_id
-    end
-
-    @item_mappings.each do |k, v|
-      @item_mappings[k][:stocks_count] = @item_mappings[k][:stock_ids].uniq.size
-      @item_mappings[k][:statements_count] = @item_mappings[k][:statement_ids].uniq.size
-    end
-
     @stocks = @item.stocks.order(:category).uniq
   end
 end
